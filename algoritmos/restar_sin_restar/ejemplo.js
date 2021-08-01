8f742ec656b3eb8540c63f6626f03a2604496067
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-08-01 18:53:26 -0500

    Exercise to subtract done

---
+++ algoritmos/restar_sin_restar/ejemplo.js
@@ -0,0 +1,4 @@
+const subtraction = (num1, num2) => num1 + (num2 * -1);
+
+subtraction(14, 10); // 4
+subtraction(-10, 14); // -24
---
+++ algoritmos/restar_sin_restar/ejemplo.py
@@ -0,0 +1,10 @@
+def subtraction(num1, num2):
+  return num1 + (num2 * -1)
+
+
+def run():
+  subtraction(14, 10) # 4
+  subtraction(-10, 14) # -24
+
+if __name__ == "__main__":
+  run()
