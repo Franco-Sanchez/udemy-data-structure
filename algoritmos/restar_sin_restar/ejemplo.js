15ac73c86e47755832bd44802eeb8cfbbfc38807
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-08-03 07:52:00 -0500

    Added solution to subtraction

--- algoritmos/restar_sin_restar/ejemplo.js
+++ algoritmos/restar_sin_restar/ejemplo.js
@@ -1,4 +1,31 @@
-const subtraction = (num1, num2) => num1 + (num2 * -1);
+const subtractionV1 = (num1, num2) => num1 + (num2 * -1);
 
-subtraction(14, 10); // 4
-subtraction(-10, 14); // -24
+const subtractionV2 = (num1, num2) => {
+    let value = 0;
+
+    if(num1 > num2) {
+        value = incrementValue(num1, num2, value);
+    } else if(num2 > num1) {
+        value = incrementValue(num2, num1, value);
+        return value * -1;
+    }
+    return value;
+};
+
+const incrementValue = (num1, num2, value) => {
+    while(num1 > num2) {
+        num2++;
+        value++;
+    }
+    return value;
+}
+
+subtractionV1(14, 10); // 4
+subtractionV1(-10, 14); // -24
+subtractionV1(20, 20); // 0
+subtractionV1(22, -22); // 44
+
+subtractionV2(14, 10); // 4
+subtractionV2(-10, 14); // -24
+subtractionV2(20, 20); // 0
+subtractionV2(22, -22); // 44
--- algoritmos/restar_sin_restar/ejemplo.py
+++ algoritmos/restar_sin_restar/ejemplo.py
@@ -1,10 +1,36 @@
-def subtraction(num1, num2):
+def subtraction_v1(num1, num2):
   return num1 + (num2 * -1)
 
 
+def subtraction_v2(num1, num2):
+  value = 0
+
+  if num1 > num2:
+    value = increment_value(num1, num2, value)
+  elif num2 > num1:
+    value = increment_value(num2, num1, value)
+    return value * -1
+  return value
+
+
+def increment_value(num1, num2, value):
+  while num1 > num2:
+      num2 += 1
+      value += 1
+  return value
+
+
 def run():
-  subtraction(14, 10) # 4
-  subtraction(-10, 14) # -24
+  subtraction_v1(14, 10) # 4
+  subtraction_v1(-10, 14) # -24
+  subtraction_v1(20, 20) # 0
+  subtraction_v1(22, -22) # 44
+
+  subtraction_v2(14, 10) # 4
+  subtraction_v2(-10, 14) # -24
+  subtraction_v2(20, 20) # 0
+  subtraction_v2(22, -22) # 44
+
 
 if __name__ == "__main__":
   run()
