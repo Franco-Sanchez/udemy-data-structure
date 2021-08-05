f208b7babe232d7649b216d3015b373e3a275146
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-08-05 00:51:54 -0500

    Added alghoritm to verify if a number is prime

---
+++ algoritmos/numero_primo/ejemplo.py
@@ -0,0 +1,14 @@
+def isPrime(num):
+    for i in range(2, num / 2 + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
